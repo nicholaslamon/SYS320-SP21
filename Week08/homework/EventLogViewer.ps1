3c582076d65a065e043995f68a319ea9c4b07d79
Nick <nicholas.lamon@mymail.champlain.edu>
2021-03-20 21:02:12 -0400

    Added the assigned scripts for week 8.

---
+++ Week08/homework/EventLogViewer.ps1
@@ -0,0 +1,14 @@
+﻿# Storyline: Review the Security Event Log.
+
+# List all the avaliable Windows Event Log logs.
+Get-EventLog -List
+
+# Create a prompt to allow user to select which log to view.
+$readLog = Read-Host -Prompt "Please select a log to review from the list above:"
+
+# Creates a prompt to allow user to enter a search phrase of their choice.
+$readPhrase = Read-Host -Prompt "Please enter search phrase for log:"
+
+#Print the log (Video 2 challenge)
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike $readPhrase } | export-csv -NoTypeInformation `
+-Path "C:\Users\test\Desktop\logs.csv"
---
+++ Week08/homework/SendEmail.ps1
@@ -0,0 +1,17 @@
+﻿# Storyline: Send an email.
+# Body of the email.
+# A variable can have an underscore or any letter or number in it.
+$msg = "Hello there."
+
+# Write host = echo
+write-host -BackgroundColor DarkRed -ForegroundColor White $msg
+
+
+# Email From address
+$email = "nicholas.lamon@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
