3b22993525fdb6cea24389c3579142aaf986383c
Nick <nicholas.lamon@mymail.champlain.edu>
2021-03-22 16:56:58 -0400

    WMI Script done and done!

---
+++ Week09/homework/WMI.ps1
@@ -0,0 +1,9 @@
+﻿# Use the Get-WMI cmdlet
+# Get-WmiObject -Class Win32_service | select Name, PathName, ProcessID
+
+# Get-WmiObject -list | where { $_.Name -ilike "Win32_[n-o]*" } | Sort-Object
+
+# Get-WmiObject -Class Win32_Account | Get-Member
+
+# Getting the network adapter information using the WMI class
+Get-WmiObject -Class Win32_NetworkAdapterconfiguration | select ServiceName, IPAddress, DefaultIPGateway, DNSDomain
