02933cf7b39b536bbde66d18e91817391ffd9716
Nick <nicholas.lamon@mymail.champlain.edu>
2021-03-22 18:02:16 -0400

    A Script to start and stop the Calculator.

---
+++ Week09/homework/CalcStartAndStop.ps1
@@ -0,0 +1,10 @@
+﻿# Storyline: A simple script to start and stop the Windows Calculator.
+
+# Starts the calculator
+Start-Process calc
+
+#This is just a wait so we can see that the calc opens
+Start-Sleep -Seconds 3
+
+# Ends the calculator process by its process name
+Stop-Process -Name "Calculator"
