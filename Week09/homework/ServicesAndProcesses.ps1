3ca25b004ef7f29667cfbb6af69d0e2f67530eb6
Nick <nicholas.lamon@mymail.champlain.edu>
2021-03-22 18:02:55 -0400

    A script to write out all running processes and services.

---
+++ Week09/homework/ServicesAndProcesses.ps1
@@ -0,0 +1,8 @@
+﻿# Storyline: Using the Get-Process and Get-Service cmdlets
+# Get-Process | Select-Object ProcessName, Path, ID | ` 
+# Export-Csv -Path "C:\users\test\Desktop\myProcesses.csv" -NoTypeInformation 
+# Get-PRocess | Get-Member
+# Get-Service | Where { $_.Status -eq "Running" }
+
+Get-Process | Select-Object ProcessName, Path, ID | Export-Csv -Path "C:\Users\test\Desktop\RunningProcesses.csv" -NoTypeInformation
+Get-Service | Where { $_.Status -eq "Running" } | Select-Object ServiceName, DisplayName | Export-Csv -Path "C:\Users\test\Desktop\RunningServices.csv" -NoTypeInformation
