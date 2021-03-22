f53ba884da67404c1990ee35eadf6e37126e25c5
Nick <nicholas.lamon@mymail.champlain.edu>
2021-03-22 16:59:30 -0400

    WMI Script done and done!

--- Week09/homework/WMI.ps1
+++ Week09/homework/WMI.ps1
@@ -1,4 +1,6 @@
-﻿# Use the Get-WMI cmdlet
+﻿# Storyline: A simple script to learn the basics of the Get-WmiObject cmdlet
+
+# Use the Get-WMI cmdlet
 # Get-WmiObject -Class Win32_service | select Name, PathName, ProcessID
 
 # Get-WmiObject -list | where { $_.Name -ilike "Win32_[n-o]*" } | Sort-Object
